ba684d76dd8e88eb9e61df65d3b41de87b67406a
cyberkyojin <cyberkyojin@gmail.com>
2022-03-16 11:19:16 -0300

    php code moved to 'set' file

--- public/index.php
+++ public/index.php
@@ -1,27 +1,4 @@
-<?php
-
-$desc = "Olá! me chamo Diego Fernandes, sou desenvolvedor back-end, entusiasta no desenvolvimento de
-    jogos e hacking. Além de estar sempre estudando alguma coisa ligada a área da programação.";
-
-$bio = "Desenvolvedor Back-end";
-
-if(isset($_GET['lang']) and $_GET['lang'] == 'portugues'){
-    $desc = "Olá! me chamo Diego Fernandes, sou desenvolvedor back-end, entusiasta no desenvolvimento de
-        jogos e hacking. Além de estar sempre estudando alguma coisa ligada a área da programação.";
-    
-    $bio = "Desenvolvedor Back-end";
-}
-
-if(isset($_GET['lang']) and $_GET['lang'] == 'english'){
-    $desc = "Hey! My name is Diego Fernandes, I'm a back-end developer. Game development and
-        hacking entusiast. And I'am always studying something related to the programming area.";
-
-    $bio = "Back-end Developer";
-
-    $english = True;
-}
-
-?>
+<?php require_once '../app/set.php'; ?>
 
 <!DOCTYPE html>
 <html lang="en">
